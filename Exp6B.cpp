a94fb0e767f6466096ae21c41681cad794733e42
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-23 01:58:29 -0400

    Create Exp6B.cpp

---
+++ Exp6B.cpp
@@ -0,0 +1,14 @@
+// EXPERIMENT 6B : Write SIT 5 times 
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0 ; i<5 ; i++){
+        cout << "SIT" << endl;
+    }
+
+    return 0;
+}
