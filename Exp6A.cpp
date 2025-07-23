468b31d531facd01799934208e94ed3178e46f96
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-23 01:55:03 -0400

    Create Exp6A.cpp

---
+++ Exp6A.cpp
@@ -0,0 +1,17 @@
+// EXPERIMENT 6A : Printing even numbers from 1 to 10
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   for (int i=0 ; i<=10 ; i++){
+       if (i%2==0){
+           cout << i << endl;
+       }
+      
+   }
+
+    return 0;
+}
