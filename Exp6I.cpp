f58c133205ee34aa08f81981ac39b84438455222
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-24 00:53:59 -0400

    Create Exp6I.cpp

---
+++ Exp6I.cpp
@@ -0,0 +1,39 @@
+// EXPERIMENT I : Inverted Star Pyramid
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+    int n= 5;
+    
+    for (i=n; i>=0 ; i--){
+        for (j=0 ; j <= n-  i ; j++){
+            cout <<  " ";
+        }
+         
+        for (k=0; k <= i; k++){
+                 cout << " *";
+            }
+        cout << endl;
+        
+    }
+
+
+    return 0;
+}
+
+/*
+
+OUTPUT :
+
+  * * * * * *
+   * * * * *
+    * * * *
+     * * *
+      * *
+       *
+
+*/
